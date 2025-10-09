018d34c7843ca5a7ff5cc4f1e0c1468487f38df9
arun5495 <esakkiarun422@gmail.com>
2025-10-09 23:42:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,17 @@
 import streamlit as st
 
-st.title('Streamlit Usage')
-
-st.header('In Mathematics')
-st.subheader('Formula:')
-formula='(a+b)2=a2+b2=2ab'
-st.latex(formula)
-
-st.header('In Programming')
-st.subheader('Code Representation:')
-code='''input=input('words:')
-words=input.split(" ")
-emoji_mapping={
-:) : 😃,
-:( : 😞}
-for word in words:
-  output+=emoji_mapping.get(word,word)
-print(output)'''
+st.title('Streamlit Usage✨')
 st.code(code,language='python')
-st.header('In Media')
-st.subheader('Image:')
-st.image('11020521.jpg',caption = "itachi",width=500)
+st.header('In Media💿')
+st.subheader('📸Image:')
+st.image('11020521.jpg',caption = " itachi",width=700)
 
-st.subheader('Video:')
-video=open('straw-hat-luffy.1920x1080.mp4','rb')
+st.subheader('🎞Video:')
+video=open('naruto.1920x1080.mp4','rb')
 video_bytes=video.read()
 st.video(video_bytes)
 
-st.subheader('Audio:')
+st.subheader('🎧Audio:')
 audio=open('Squid Game Season 2 Mingle Mp3','rb')
 audio_bytes=audio.read()
+st.audio(audio_bytes)
