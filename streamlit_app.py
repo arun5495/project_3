606a9833a434cd057f0e3b6e791b5b2807f848a4
arun5495 <esakkiarun422@gmail.com>
2025-10-09 23:14:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("📁 Load Local Files")
 # Example local paths (change these to match your own)
 image_path = images.jpg
 video_path = download.jpg
-audio_path =audio file
+audio_path =
 
 # Display image")
 st.subheader("images.jpg")
@@ -17,6 +17,6 @@ with open(video_path, "rb") as v:
     st.video(v.read())
 
 # Display audio
-st.subheader("audio file")
+st.subheader("")
 with open(audio_path, "rb") as a:
     st.audio(a.read())
