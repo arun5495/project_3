d2fe23d68b5553dd3ec4ee16e4be85e6b51fc7bd
arun5495 <esakkiarun422@gmail.com>
2025-10-09 23:08:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,18 @@ st.title("📁 Load Local Files")
 # Example local paths (change these to match your own)
 image_path = images.jpg
 video_path = download.jpg
-audio_path = "C:/Users/YourName/Music/my_song.mp3"
+audio_path =Squid Game Season 2 Ringtone Mingle Mp3 Download - MobCup.Com.Co.mp3
 
-# Display image
-st.subheader("🖼️ Image from Local Path")
+# Display image")
+st.subheader("images.jpg")
 st.image(image_path, caption="Local Image", use_column_width=True)
 
 # Display video
-st.subheader("🎥 Video from Local Path")
+st.subheader("download.jpg")
 with open(video_path, "rb") as v:
     st.video(v.read())
 
 # Display audio
-st.subheader("🎵 Audio from Local Path")
+st.subheader("Squid Game Season 2 Ringtone Mingle Mp3 Download - MobCup.Com.Co.mp3")
 with open(audio_path, "rb") as a:
     st.audio(a.read())
